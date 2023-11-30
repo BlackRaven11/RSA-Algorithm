b60f308880121458790dfaddcac9184f0c09d710
BlackRaven11 <blackcorvum@gmail.com>
2023-11-30 22:39:07 +0330

    save private key into file set

--- RSA_Algorithm/kegen.py
+++ RSA_Algorithm/kegen.py
@@ -2,6 +2,15 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 #Generate RSA key pair
 private_key = rsa.generate_private_key(
-    public_exponent = 65537
+    public_exponent = 65537,
     key_size= 2048
 )
+public_key = private_key.public_key()
+
+#save private key to file
+with open("private_key.pem" , "wb") as key_file:
+    key_file.write(private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    ))
