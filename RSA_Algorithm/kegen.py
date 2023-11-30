15b2ce9902cd913ada712058dd749738bab8c675
BlackRaven11 <blackcorvum@gmail.com>
2023-12-01 01:37:16 +0330

    edit

--- RSA_Algorithm/kegen.py
+++ RSA_Algorithm/kegen.py
@@ -1,5 +1,6 @@
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
+
 #Generate RSA key pair
 private_key = rsa.generate_private_key(
     public_exponent = 65537,
