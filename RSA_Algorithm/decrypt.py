ba03c52c2a8d7ce349f94847e37bb8b22470f5a2
BlackRaven11 <blackcorvum@gmail.com>
2023-12-01 02:11:06 +0330

    loading from file and printing the message

--- RSA_Algorithm/decrypt.py
+++ RSA_Algorithm/decrypt.py
@@ -12,3 +12,21 @@ def decrypt_message(encrypted_message,private_key):
         )
     )
     return decrypted_message.decode()
+
+#Load private key from file
+with open("private_key.pem" , "rb") as key_file:
+    private_key =  serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+        backend=None
+    )
+
+#load encrypted message from file 
+with open("encrypted_message.txt" , "rb") as file:
+    encrypted_message = file.read()
+
+#Decrypt the message
+decrypted_message = decrypt_message(encrypted_message , private_key)
+
+#print the decrypted message
+print("Decrypted message: " , decrypted_message)
