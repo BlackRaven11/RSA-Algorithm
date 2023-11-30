fcea2675c0de20aba6a62c7eb45f57b60e5ebe58
BlackRaven11 <blackcorvum@gmail.com>
2023-11-30 19:28:05 +0330

    commenting

--- RSA_Algorithm/encrypt.py
+++ RSA_Algorithm/encrypt.py
@@ -5,8 +5,10 @@ from cryptography.hazmat.primitives import hashes
 
 def encrypt_message(message , public_key): #encrypting the message using public key
     encrypted_message = public_key.encrypt(
-        message.encode(),
-        padding.OAEP(
+        message.encode(), #the encode() is converting the message into bytes
+        #the OAEP : Optimal Asymmtric Encryption Padding is to perform the encryption by scheme
+        #which provides probabilistic encryption and enhances security
+        padding.OAEP(   
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm = hashes.SHA256(),
             label=None
