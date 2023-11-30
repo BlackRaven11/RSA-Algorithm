edd41ad192d909c220ffa1f0a358886633178d13
BlackRaven11 <blackcorvum@gmail.com>
2023-11-30 19:16:06 +0330

    encrypt_message function coded

--- RSA_Algorithm/encrypt.py
+++ RSA_Algorithm/encrypt.py
@@ -2,3 +2,14 @@
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa,padding
 from cryptography.hazmat.primitives import hashes
+
+def encrypt_message(message , public_key):
+    encrypted_message = public_key.encrypt(
+        message.encode(),
+        padding.OAEP(
+            mgf=MGF1(algorithm=hashes.SHA256()),
+            algorithm = hashes.SHA256(),
+            label=None
+        )
+    )
+    return encrypted_message
